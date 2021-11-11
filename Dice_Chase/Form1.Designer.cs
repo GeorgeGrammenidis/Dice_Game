3c1001b538fce62b5c7487cfca2fab0f068faedc
GeorgeGrammenidis <61099352+GeorgeGrammenidis@users.noreply.github.com>
2021-11-11 16:51:41 +0200

    Add files via upload

---
+++ Dice_Chase/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Dice_Chase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(485, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(349, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Score:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(641, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Normal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(721, 30);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "Hard";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(570, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Difficulty:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1296, 86);
+            this.panel1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1180, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 30);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Highest Score";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1058, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Score List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Your Name:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(239, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(170, 40);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1006, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(806, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(182, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Save Score When Done";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1180, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 28);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1320, 516);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Dice_Chase/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Dice_Chase
+{
+    public partial class Form1 : Form
+    {
+        int counter=0; //how many times are remaining for the dice in the current round//
+        int score = 0; //the user's total amount of points in a round//
+        int X;//the coordinates of the dice//
+        int Y;
+        int value; //the amount of points each side of the dice is worth//
+        bool check=true; // whether the dice has already been clicked in its current state//
+        int record = 0; //the highest score of any user on the list//
+        string champion;//the name of the player with the highest score//
+        Random rng = new Random(); //a random number generator//
+        public Form1()
+        {
+            InitializeComponent();
+            //initializes the form, giving the dice, the label and the user name a default state//
+            pictureBox1.ImageLocation = "dice1.png";
+            label1.Text = "0";
+            richTextBox1.Text = "Anonymous User";
+            label4.Text = "0";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            check = true; // this signifies that the dice can be clicked to gather more points in the current state//
+            label1.Text = score.ToString(); //this label is always updated with the user's current score//
+            label4.Text = (20 - counter).ToString(); //shows how many more "dice throws" are left//
+            counter = counter + 1; //how many times the dice has changed in a current round//
+            //the following lines pick a random new location for the dice//
+            X = rng.Next(0, 1025);
+            Y = rng.Next(100, 545);
+            pictureBox1.Location = new Point(X, Y);
+            value = rng.Next(1 , 7); //picks a random value from 1 to 6, meaning how many points the user will get if they click it//
+            pictureBox1.ImageLocation = "dice" + value.ToString() + ".png"; //gives the dice the appropriate image//
+
+            if (counter > 20) //the game stops after 20 random dice movements//
+            {
+                timer1.Stop(); //stops the timer//
+                MessageBox.Show("Your score was: " + score); //shows score//
+                if (checkBox1.Checked == true) //if the checkbox is ticked then the game saves the score in a text file//
+                {
+                    StreamWriter sw = new StreamWriter("list.txt", true); //opens the text file to write//
+                    if (radioButton1.Checked == true) //along with the score and name it also saves the difficulty the user had picked//
+                    {
+                        //saves score, name and difficulty//
+                        sw.WriteLine(score.ToString() + " by " + richTextBox1.Text + " Difficulty: Normal ");
+                    }
+                    else if (radioButton2.Checked == true)
+                    {
+                        sw.WriteLine(score.ToString() + " by " + richTextBox1.Text + " Difficulty: Hard ");
+                    }
+                    
+                    sw.Close();
+                }
+            }
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //depending on the difficulty picked the timer interval is altered so the dice will move faster//
+            if (radioButton1.Checked == true)
+            {
+                timer1.Interval = 1500;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                timer1.Interval = 1000;
+            }
+            //everytime the game starts all the following variables need to be reset//
+            score = 0;
+            counter = 0;
+            label1.Text = "0";
+            timer1.Start(); //the timer starts//
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (check == true) { //this makes sure that the dice can only be clicked once between intervals//
+                int multiplier = 1;
+                if (radioButton2.Checked == true) { multiplier = 2; } //if the user has chosen the hard difficulty they are rewarded with more points//
+                score = score + multiplier*value;
+                check = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //when the difficulty is changed midgame, everything resets//
+            timer1.Stop();
+            score = 0;
+            label1.Text = "0";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            score = 0;
+            label1.Text = "0";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //if the checkbox is changed midgame everything resets//
+            timer1.Stop();
+            score = 0;
+            label1.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            score = 0;
+            counter = 0;
+            //if this button is pressed the text file with the list of scores is shown to the user//
+            StreamReader sr = new StreamReader("list.txt");
+            try
+            {
+                string s = sr.ReadLine();
+                StringBuilder sb = new StringBuilder();
+                while (s != null)
+                {
+                    sb.Append(s + "\n");
+                    s = sr.ReadLine();
+                }
+                MessageBox.Show(sb.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if this button is pressed the program attempts to find the highest score on the list//
+            try
+            {
+                StreamReader sr = new StreamReader("list.txt");
+                string information = sr.ReadLine(); //the program checks each individual line//
+
+                while (information != null)
+                {
+                    string[] words = information.Split();
+                    string help = words[0];
+
+                    int current_score = Convert.ToInt32(help); //the first word of each line is the score//
+                    if (current_score > record)
+                    {
+                        record = current_score; //the record is replaced if a higher one is found//
+                        champion = words[2];
+                    }
+                    information = sr.ReadLine();
+                }
+                MessageBox.Show(record.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
